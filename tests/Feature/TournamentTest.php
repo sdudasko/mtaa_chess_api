d4bd30da6b6a13c0f7b7c6bc2530a6e472c5ffa2
freeset <peter.demeter98@gmail.com>
2021-04-01 18:14:50 +0200

    Major changes

--- tests/Feature/TournamentTest.php
+++ tests/Feature/TournamentTest.php
@@ -13,6 +13,18 @@ class TournamentTest extends TestCase
 
     public function test_organizer_can_create_tournament()
     {
+        $administrator = User::where('role_id', 1)->first();
+        $this->actingAs($administrator, 'api');
+
+        $param=[];
+
+        $param['title']='title';
+        $param['tempo_minutes']='3';
+
+
+
+        $response = $this->call('put', "v1/tournaments",  $param);
+
         $this->assertTrue(true);
     }
 
