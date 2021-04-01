f525d3d62a66df15c03889651eb36c074bf33290
freeset <peter.demeter98@gmail.com>
2021-04-01 19:23:28 +0200

    Finishing final version of assingnment

--- tests/Feature/MatchTest.php
+++ tests/Feature/MatchTest.php
@@ -91,6 +91,17 @@ class MatchTest extends TestCase
 
     public function test_organizer_cannot_start_new_round_if_all_games_are_not_completed()
     {
-     
+        Artisan::call('db:seed');
+
+        $administrator = User::where('role_id', 1)->first();
+        $this->actingAs($administrator, 'api');
+        $tournament_id = Tournament::first()->id;
+
+        $response = $this->put( "v1/matches/");
+
+        $response_better = $this->put( "v1/matches/");
+        dd(Match::where("result",null)->count());
+        dd($response_better->status());
+        $this->assertTrue(true);
     }
 }
